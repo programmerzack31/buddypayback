2af77d1257e575fe42f865d43de04bec288a13e9
Muhammad Zeeshan <zeeshanbhaiyaa@gmail.com>
2025-08-05 23:28:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const notificationRoutes = require("./routes/notificationRoute");
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "https://buddypay-one.vercel.app/"}));
+app.use(cors({ origin: "*" , credentials: true}));
 const server = http.createServer(app)
 initSocket(server);
 
